f8489fd179c73e1d57f8efb386957d4a7a8a5ed8
vasilivlas <vasilivlas95@gmail.com>
2025-08-25 19:30:29 +0700

    Upload tests

---
+++ src/main/java/ReqresTests.java
@@ -0,0 +1,125 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReqresTests {
+
+    @Test
+    public void getSingleResource() {
+        ValidatableResponse response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .queryParam("id", "1")
+                .when()
+                .get("https://reqres.in/api/unknown/")
+                .then()
+                .statusCode(200);
+
+        int statusCode = response.extract().statusCode();
+        Assertions.assertEquals(200, statusCode);
+
+        System.out.println("Response: " + "\n" + response.extract().asPrettyString());
+        System.out.println("Status code: " + response.extract().statusCode());
+    }
+
+
+    @Test
+    public void createUserTest() {
+        Map<String, String> body = new HashMap<>();
+        body.put("name", "neo");
+        body.put("job", "savior");
+
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users")
+                .andReturn();
+        response.prettyPrint();
+        String actualName = response.jsonPath().getString("name");
+        String actualJob = response.jsonPath().getString("job");
+        Assertions.assertEquals("neo", actualName);
+        Assertions.assertEquals("savior", actualJob);
+
+
+        int userId = response.jsonPath().getInt("id");
+
+        System.out.println("User ID: " + userId);
+    }
+
+    @Test
+
+    public void updateUserProfile() {
+
+        Map<String, String> body = new HashMap<>();
+        body.put("name", "neo");
+        body.put("job", "ordinary man");
+
+        Response targetResponse = RestAssured
+
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/users/960")
+                .andReturn();
+        targetResponse.prettyPrint();
+
+        String actualJob = targetResponse.jsonPath().getString("job");
+        Assertions.assertEquals("ordinary man", actualJob);
+
+
+    }
+
+    @Test
+    public void deleteUserTest() {
+        Map<String, String> body = new HashMap<>();
+        body.put("name", "neo");
+        body.put("job", "savior");
+
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(body)
+                .when()
+                .delete("https://reqres.in/api/users/960")
+                .andReturn();
+
+        int statusCodeValue = response.statusCode();
+
+        Assertions.assertEquals(204, statusCodeValue);
+        System.out.println("Status code: " + response.statusCode());
+    }
+
+
+    @Test
+    public void loginUnsuccessfulTest() {
+        Map<String, String> body = new HashMap<>();
+        body.put("e-mail", "neo@theone.com");
+
+        Response response = RestAssured
+                .given()
+                .header("x-api-key", "reqres-free-v1")
+                .contentType("application/json")
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/login")
+                .andReturn();
+        response.prettyPrint();
+
+        int statusCodeValue = response.statusCode();
+        Assertions.assertEquals(400, statusCodeValue);
+
+        System.out.println();
+        System.out.println("Status code: " + statusCodeValue);
+    }
+}
